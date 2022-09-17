013218a9c1f3f5a9c58d12b565b22bce610f3fb7
User <dg21stew@gmail.com>
2022-09-16 23:59:15 -0700

    Post and Get working

--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-@Repository
+@Repository("fakeDao")
 public class FakePersonDataAccessService implements PersonDao{
 
     private static List<Person> DB = new ArrayList<>();
@@ -17,6 +17,11 @@ public class FakePersonDataAccessService implements PersonDao{
     public int insertPerson(UUID id, Person person) {
         DB.add(new Person(id, person.getName()));
         return 1;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        return DB;
     }
 
 
